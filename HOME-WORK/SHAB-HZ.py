88d336391b9a86e95ebb35f04d52f3b1a46b96f2
BatyrKot <113506223+BatyrKot@users.noreply.github.com>
2022-11-05 12:00:03 +0300

    Delete HOME-WORK directory

--- HOME-WORK/SHAB-HZ.py
+++
@@ -1,9 +0,0 @@
-from Dez import *
-def create_list(*args, **kwargs):
-    DiskC=[]
-    for zn1,zn2 in enumerate(args):
-        DiskC.append(f"Name_{zn1} = {deg_to_gms(zn2)}")
-    for zk1,zk2 in kwargs.items():
-        DiskC.append(f"{zk1} = {deg_to_gms(zk2)}")
-    return DiskC
-print(create_list(172.258943,321.123123,12.321321,pole=21.123123,put_1=123123123))
